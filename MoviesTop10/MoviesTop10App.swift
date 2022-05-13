eadc1f69fe7ab092f878cbdf797aae9555775bb6
Sushi57 <tiwari23sakshi@gmail.com>
2022-05-13 14:08:40 +0530

    First Commit, Blank Project Added

---
+++ MoviesTop10/MoviesTop10App.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesTop10App.swift
+//  MoviesTop10
+//
+//  Created by SAKSHI TIWARI on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesTop10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
