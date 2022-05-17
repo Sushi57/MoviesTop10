efa68cf5f49a43c24f245f976a6794a140a53445
Sushi57 <tiwari23sakshi@gmail.com>
2022-05-18 00:04:33 +0530

    Extra code removed

--- MoviesTop10/Utilities/Extensions.swift
+++ MoviesTop10/Utilities/Extensions.swift
@@ -28,26 +28,3 @@ func hexStringToUIColor (hex:String) -> UIColor {
     )
 }
 
-//MARK: - Boxing Function
-
-final class Box<T> {
-    
-    typealias Listener = (T) -> Void
-    var listener: Listener?
-    
-    var value: T {
-        didSet {
-            listener?(value)
-        }
-    }
-    
-    init(_ value: T) {
-        self.value = value
-    }
-    
-    func bind(listener: Listener?) {
-        self.listener = listener
-        listener?(value)
-    }
-}
-
